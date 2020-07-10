879a90e0e0b6899a40d910db549bc522f5d72e5a
Instance <mycloudndevops@gmail.com>
2020-07-10 13:40:47 +0000

    removed the old code

--- scripts/bootstrap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo yum -y update
-sudo yum install -y wget vim tar ksh git
-sudo amazon-linux-extras install ansible2
-sudo mkdir /apps
-sudo chmod 777 /apps
-# you can dowload the latest playbook packages from artifactory or s3. This playbooks you can keep it in seprate repository for your development and push the latest packages always to artifactory. so that it can download the latest changes and install by ansible instead of running from local executor
